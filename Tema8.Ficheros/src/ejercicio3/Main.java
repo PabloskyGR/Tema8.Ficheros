1811df9bd701d34085dd2b83d52a4319e1b1e815
jpgonzalez <jpgonzalez@106-31.info2>
2024-05-09 13:17:39 +0200

    hacer ejercicio 3

---
+++ Tema8.Ficheros/src/ejercicio3/Main.java
@@ -0,0 +1,20 @@
+package ejercicio3;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		try {
+			FileReader rutaFichero = new FileReader("src/ejercicio3/Alumnos.txt");
+
+			BufferedReader bf = new BufferedReader(rutaFichero);
+		} catch (FileNotFoundException e) {
+			System.err.println(e.getMessage());
+		}
+	}
+
+}
