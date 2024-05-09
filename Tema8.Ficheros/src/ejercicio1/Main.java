0523b2dbea6ed9fe5b25c271e70091d8b08d71a0
jpgonzalez <jpgonzalez@106-31.info2>
2024-05-09 12:57:01 +0200

    corregir el 1

--- Tema8.Ficheros/src/ejercicio1/Main.java
+++ Tema8.Ficheros/src/ejercicio1/Main.java
@@ -18,15 +18,16 @@ public class Main {
 			double media = 0;
 			int suma = 0;
 			int cont = 0;
-			String linea = "";
+			String linea = bf.readLine();
 
-			while (bf.readLine() != null) {
-				suma += bf.read();
+			while (linea != null) {
+				suma += Integer.parseInt(linea);
 				cont++;
+				linea = bf.readLine();
 			}
 
 			if (cont > 0) {
-				media = suma / cont;
+				media = (double) suma / cont;
 				System.out.println("La suma es: " + suma);
 				System.out.println("La media es: " + media);
 			} else {
@@ -38,6 +39,8 @@ public class Main {
 		} catch (FileNotFoundException e) {
 			System.err.println("Archivo no encontrado: " + e.getMessage());
 		} catch (IOException e) {
+			System.err.println(e.getMessage());
+		} catch (NumberFormatException e) {
 			System.err.println(e.getMessage());
 		}
 
